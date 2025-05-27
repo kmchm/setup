cbd8d0d51ca5e4606d9668d385539a327b196f05
Kamil <mail@kamilch.com>
2025-05-27 12:26:17 +0200

    Add dock magnification settings

--- mac.sh
+++ mac.sh
@@ -14,6 +14,8 @@ defaults write com.apple.dock autohide-time-modifier -float 0.7
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock tilesize -int 46
+defaults write com.apple.dock largesize -float 46
+defaults write com.apple.dock magnification -bool true
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock mineffect -string "genie"
 defaults write com.apple.dock mru-spaces -bool false
